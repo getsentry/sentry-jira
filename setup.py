2ace9b0b46a116a34f995c9f29890bcb98fa6a6b
Thurloat <thurloat@gmail.com>
2012-09-11 17:03:26 -0300

    releasing 0.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ f.close()
 
 setup(
     name='sentry-jira',
-    version='0.5.8',
+    version='0.6.0',
     author='Adam Thurlow',
     author_email='thurloat@gmail.com',
     url='http://github.com/thurloat/sentry-jira',
