41675c496ce1448ab0df30ddcb6492a48a970d69
Thurloat <thurloat@gmail.com>
2012-10-03 16:22:02 -0300

    0.6.5 damnit

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ f.close()
 
 setup(
     name='sentry-jira',
-    version='0.6.4',
+    version='0.6.5',
     author='Adam Thurlow',
     author_email='thurloat@gmail.com',
     url='http://github.com/thurloat/sentry-jira',
