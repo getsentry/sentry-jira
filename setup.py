70aff574bd44302d313cbcf63c99fd8616cf5be2
Thurloat <thurloat@gmail.com>
2012-11-14 12:59:16 -0400

    bump version number to include bugfix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ f.close()
 
 setup(
     name='sentry-jira',
-    version='0.6.9',
+    version='0.6.11',
     author='Adam Thurlow',
     author_email='thurloat@gmail.com',
     url='http://github.com/thurloat/sentry-jira',
