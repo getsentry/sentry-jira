ee1156aec635c514f804cce304b7f7c01ae39364
Thurloat <thurloat@gmail.com>
2013-01-30 23:41:48 -0400

    lets just bump versions

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ f.close()
 
 setup(
     name='sentry-jira',
-    version='0.7.0',
+    version='0.7.1',
     author='Adam Thurlow',
     author_email='thurloat@gmail.com',
     url='http://github.com/thurloat/sentry-jira',
